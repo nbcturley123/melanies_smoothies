883bbfdb21281697ddba36ca4a882b591c0ddf7c
nbcturley123 <74419191+nbcturley123@users.noreply.github.com>
2024-11-25 11:12:23 -0500

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 import requests
-import Pandas as pd
+import pandas as pd
 
 cnx = st.connection("snowflake")
 session = cnx.session()
